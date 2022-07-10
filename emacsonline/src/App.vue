2363d0e91b2fa7a96b26cf1f3aa13bbac4213157
Hee Hwang <heehcs@gmail.com>
2022-07-10 07:40:41 -0400

    Set up yarn, vue, and monaco-editor

---
+++ emacsonline/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<MainEditor/>
+</template>
+
+<script>
+import MainEditor from './components/MainEditor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainEditor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ emacsonline/src/components/MainEditor.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="editor" style="width: 1280px; height: 1024px"></div>
+</template>
+
+<script>
+import loader from "@monaco-editor/loader";
+
+export default {
+ name: "MainEditor",
+ async mounted() {
+
+   loader.init().then((monaco) => {
+
+     monaco.editor.create(document.getElementById("editor"));
+    
+   });
+  
+ },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
